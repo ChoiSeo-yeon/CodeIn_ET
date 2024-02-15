8ea12e9feae6bbb465c257336dc6bbe9b8603110
daaexkim <jennyxdaae@gachon.ac.kr>
2024-02-15 20:11:20 +0900

    Feat

--- Frontend/eyetracking-project/src/components/Test.jsx
+++ Frontend/eyetracking-project/src/components/Test.jsx
@@ -24,17 +24,29 @@ function Test() {
     }
   }, [circles]);
 
-  const handleClick = (id) => {
+  const handleClick = (id, top, left) => {
     if (!gameOver) {
       if (!gameStarted) {
         setGameStarted(true);
       }
+
+      // 그리드 칸 크기 계산
+      const gridWidth = window.innerWidth / 5;
+      const gridHeight = window.innerHeight / 4;
+
+      // 클릭한 동그라미의 위치를 그리드 칸 번호로 변환
+      const column = Math.floor(left / gridWidth) + 1;
+      const row = Math.floor(top / gridHeight) + 1;
+      const cellNumber = (row - 1) * 5 + column;
+
+      console.log(`Clicked circle position: ${cellNumber}`);
+
       const updatedCircles = circles.map((circle) =>
         circle.id === id ? { ...circle, isVisible: false } : circle
       );
       setCircles(updatedCircles);
       setScore((prevScore) => prevScore + 1);
-  
+
       if (score + 1 === 10) {
         setGameOver(true);
       } else {
@@ -77,7 +89,7 @@ function Test() {
             key={circle.id}
             className="circle"
             style={{ top: circle.top, left: circle.left }}
-            onClick={() => handleClick(circle.id)}
+            onClick={() => handleClick(circle.id, circle.top, circle.left)}
           ></div>
         )
       ))}
